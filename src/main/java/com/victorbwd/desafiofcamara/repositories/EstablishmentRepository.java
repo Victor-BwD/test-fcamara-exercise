4ead4084a33ee71be54faeb0185a6f112f5bb267
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2024-01-22 17:00:38 -0300

    Repositories

---
+++ src/main/java/com/victorbwd/desafiofcamara/repositories/EstablishmentRepository.java
@@ -0,0 +1,10 @@
+package com.victorbwd.desafiofcamara.repositories;
+
+import com.victorbwd.desafiofcamara.domain.establishment.Establishment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstablishmentRepository extends MongoRepository<Establishment, String> {
+
+}
